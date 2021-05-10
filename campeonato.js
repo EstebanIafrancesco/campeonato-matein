5c835756a928c3ed7c6222d546f4863ee8854c19
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-05-09 19:56:18 -0500

    Basic structure for the project, car championship simulator for kids

---
+++ campeonato.js
@@ -0,0 +1,13 @@
+console.log("Estoy en el campeonato de Matein ...");
+let arrayCompetidores = [
+  {
+    nombre: "Mclaren F1 GTR",
+    foto: "./carros/mclaren-f1-gtr.jpeg",
+  },
+];
+// let nombreCompetidor = `<h1>${arrayCompetidores[0].nombre}</h1>`;
+// let fotoCompetidor = `<img class="fotos" src="${arrayCompetidores[0].foto}" alt="${[0].arrayCompetidores[0].nombre}" />`;
+
+document.getElementById("sifun").innerHTML = <h1>pave si funciona</h1>;
+// document.getElementById("").innerHTML = fotoCompetidor;
+
